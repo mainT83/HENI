9aa43b5af488e3607e722969acec37348e8a18ab
Heni Gattoussi <maintenance@lesultan.com>
2026-08-27 10:33:03 +0100

    Revert "Intègre le paiement Konnect pour le passage au plan premium"
    This reverts commit c88148829306ea2f40624cb81dadc9957105fa5f.

--- lib/providers/payment_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../data/payment_repository.dart';
-import 'supabase_provider.dart';
-
-final paymentRepositoryProvider = Provider<PaymentRepository>((ref) {
-  return PaymentRepository(ref.watch(supabaseClientProvider));
-});
